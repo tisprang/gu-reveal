a90f208ad3e0c0d430c29af2e485f8da61e619a7
Timo Sprang <sprang@soz.uni-frankfurt.de>
2025-03-04 16:43:40 +0100

    breakthrough

--- slide-contents-wrap.js
+++ slide-contents-wrap.js
@@ -20,27 +20,6 @@ window.RevealSlideContentsWrap = function () {
             wrapper.appendChild(child);
           });
           slide.appendChild(wrapper);
-
-          // Define a function to dynamically resize the wrapper to fit the slide
-          const resizeSlide = function () {
-            // Calculate the slide height as a fixed value (700) minus the height of the first child element
-            const slideHeight = 700 - firstChild.offsetHeight;
-
-            // Get the current height of the wrapper div
-            const wrapperheight = wrapper.offsetHeight;
-
-            // Calculate the scale factor needed to fit the wrapper content inside the slide height
-            const scaleFactor = wrapperheight > slideHeight ? slideHeight / wrapperheight : 1;
-
-            // Apply the scale transform to the wrapper div
-            wrapper.style.transform = `scale(${scaleFactor})`;
-            wrapper.style.transformOrigin = 'top left';
-          };
-
-          // Bind the resize function to the window load, resize, and slidechanged events
-          window.addEventListener('load', resizeSlide);
-          window.addEventListener('resize', resizeSlide);
-          deck.on('slidechanged', resizeSlide);
         }
       });
     },
