559df80d712a33ef3541309882e8e05eb12d08b1
tisprang <timo.s1903@googlemail.com>
2025-03-05 14:18:12 +0100

    huge progress

--- _extensions/gu-reveal/_extensions/g-simmons/slide-contents-wrap/slide-contents-wrap.js
+++ _extensions/gu-reveal/_extensions/g-simmons/slide-contents-wrap/slide-contents-wrap.js
@@ -8,17 +8,21 @@ window.RevealSlideContentsWrap = function () {
 
       // For each slide, get its children and the first child element
       slides.forEach((slide) => {
-        const children = slide.children;
+        const children = Array.from(slide.children);
         const firstChild = children[0];
 
         // If the first child element is an H2 header, wrap the rest of the children in a div
-        if (firstChild.tagName === "H2") {
-          const otherChildren = Array.from(children).slice(1);
+        if (firstChild && firstChild.tagName === "H2") {
           const wrapper = document.createElement("div");
           wrapper.classList.add("slide-contents-wrap");
-          otherChildren.forEach((child) => {
-            wrapper.appendChild(child);
+
+          children.slice(1).forEach((child) => {
+            // Only append children that do not have the 'auths' class
+            if (!child.classList.contains("auths")) {
+              wrapper.appendChild(child);
+            }
           });
+
           slide.appendChild(wrapper);
         }
       });
